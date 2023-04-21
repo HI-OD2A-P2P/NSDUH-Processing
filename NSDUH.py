d351b705caa7da09b63713945e44a773aa7fba67
jgeis <jgeis26@gmail.com>
2023-04-20 14:42:42 -1000

    Added rest of the years.  Needed new weight and filter values.

--- NSDUH.py
+++ NSDUH.py
@@ -8,14 +8,8 @@ import json
 import geopandas as gpd
 import csv
 
-
-# TODO: 
-# - need to get rid of duplicates, may need to change dict to write out a csv 
-#   line instead and insert that into a set as a string.
-# - add years/timeframe to the csv/dict/set
-# - add in other years to state data
-# - clean up!
-#
+#import mysql.connector as msql
+#from mysql.connector import Error
 
 
 # when I installed #pip-system-certs, everything else broke with 
@@ -88,9 +82,6 @@ json_path = dir + json_file_in
 csv_file = "temp.csv"
 csv_path = dir + csv_file
 
-
-county_control = "STCTYCOD2" #  Collapsed State County Code = {15: HI, 001: Hawaii County, 15: HI, 003: Honolulu County, 15: HI 005: Kauai County, 15: HI 007: Maui County}
-state_control = "STNAME"
 """
 rows (AKA demographics):
 CATAG2 Rc-Age Category Recode (3 Levels)
@@ -112,31 +103,49 @@ AMHTXND2 Rc-Perceived Unmet Need/did Not Rcv Mh Trt In Pst Yr
 """
 county_columns = ["ABODMRJ","ABODALC","ABODHER","ABODCOC","AMIYR_U","SMIYR_U","AMHTXRC3","AMHTXND2"]
 state_columns = ["ABODMRJ","ABODALC","ABODHER","ABODCOC","UDPYIEM","UDPYILL","UDPYMTH","UDPYOPI","UDPYHRPNR","AMIYR_U","SMIYR_U","AMHTXRC3","AMHTXND2","AMISUDPY","SMISUDPY","TXYRRECVD2"]
-county_filter = "STCTYCOD2%3D85%2C83%2C84%2C82"
-state_filter = "STNAME%3DHAWAII"
-state_years = ["NSDUH-2018-2019-RD02YR","NSDUH-2016-2017-RD02YR","NSDUH-2014-2015-RD02YR","NSDUH-2012-2013-RD02YR","NSDUH-2010-2011-RD02YR","NSDUH-2008-2009-RD02YR","NSDUH-2006-2007-RD02YR","NSDUH-2004-2005-RD02YR","NSDUH-2002-2003-RD02YR"]
-#state_years = [{"start":"2018","end":"2019","length":"02","title":"2018-2019"}]
-#county_years = ["NSDUH-2010-2019-RD10YR", ]
 
-counties = {"82":"Hawaii County", "83":"Honolulu County","84":"Kauai County","85":"Maui County"}
-state_weight = "DASWT_1"
-county_weight = "DASWT_4"
-county_base_url = "https://rdas.samhsa.gov/api/surveys/NSDUH-2010-2019-RD10YR/crosstab/"
-state_base_url = "https://rdas.samhsa.gov/api/surveys/NSDUH-2018-2019-RD02YR/crosstab/"
+state_years = [{"url_term":"NSDUH-2018-2019-RD02YR","year_range":"2018-2019","start_year":"2018","end_year":"2019", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2016-2017-RD02YR","year_range":"2016-2017","start_year":"2016","end_year":"2017", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2014-2015-RD02YR","year_range":"2014-2015","start_year":"2014","end_year":"2015", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2012-2013-RD02YR","year_range":"2012-2013","start_year":"2012","end_year":"2013", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2010-2011-RD02YR","year_range":"2010-2011","start_year":"2010","end_year":"2011", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2008-2009-RD02YR","year_range":"2008-2009","start_year":"2008","end_year":"2009", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2006-2007-RD02YR","year_range":"2006-2007","start_year":"2006","end_year":"2007", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2004-2005-RD02YR","year_range":"2004-2005","start_year":"2004","end_year":"2005", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"},
+               {"url_term":"NSDUH-2002-2003-RD02YR","year_range":"2002-2003","start_year":"2002","end_year":"2003", "num_years":"2", "weight":"DASWT_1", "control":"STNAME","filter":"STNAME%3DHAWAII"}]
 
-# TODO: working here.  Change results from an array of dicts to a set of strings.
-# edit the writing process to handle it.
-# edit: makeCellDict, load_state_and_county_data, parse_data (append -> add), and write_json_to_csv_file
+county_years = [{"url_term":"NSDUH-2010-2019-RD10YR","year_range":"2010-2019","start_year":"2010","end_year":"2019", "num_years":"10", "weight":"DASWT_4", "control":"STCTYCOD2","filter":"STCTYCOD2%3D85%2C83%2C84%2C82"},
+                {"url_term":"NSDUH-2002-2017-RD16YR","year_range":"2002-2017","start_year":"2002","end_year":"2017", "num_years":"16", "weight":"DASWT_8", "control":"STCTYCOD","filter":"STCTYCOD%3D74%2C75%2C76%2C77"},
+                {"url_term":"NSDUH-2002-2016-RD15YR","year_range":"2002-2016","start_year":"2002","end_year":"2016", "num_years":"15", "weight":"DASWT_7", "control":"STCTYCOD","filter":"STCTYCOD%3D74%2C75%2C76%2C77"},
+                {"url_term":"NSDUH-2002-2015-RD14YR","year_range":"2002-2015","start_year":"2002","end_year":"2015", "num_years":"14", "weight":"DASWT_6", "control":"STCTYCOD","filter":"STCTYCOD%3D74%2C75%2C76%2C77"},
+                {"url_term":"NSDUH-2002-2013-RD12YR","year_range":"2002-2013","start_year":"2002","end_year":"2013", "num_years":"12", "weight":"DASWT_5", "control":"STCTYCOD","filter":"STCTYCOD%3D74%2C75%2C76%2C77"},
+                {"url_term":"NSDUH-2002-2011-RD10YR","year_range":"2002-2011","start_year":"2002","end_year":"2011", "num_years":"10", "weight":"DASWT_4", "control":"STCTYCOD","filter":"STCTYCOD%3D74%2C75%2C76%2C77"}]
+
+# turns out the older years use different values for the counties than the latest.
+# fortunately, there was no overlap, so I just put them all in the same dict for simplicity
+counties = {"74":"Hawaii County", "75":"Honolulu County","76":"Maui County","77":"County Not Specified","82":"Hawaii County", "83":"Honolulu County","84":"Kauai County","85":"Maui County"}
+#counties = {"74":"Hawaii County", "75":"Honolulu County","76":"Maui County","77":"County Not Specified"}
+#counties = {"82":"Hawaii County", "83":"Honolulu County","84":"Kauai County","85":"Maui County"} # orig
+
+
+"""
+# this one fails because the column AMIYR_U doesn't exist for this year group
+index: 5, control: STCTYCOD, row: CATAG2, column: AMIYR_U,  filter: STCTYCOD%3D74%2C75%2C76%2C77, weight: DASWT_8
+https://rdas.samhsa.gov/api/surveys/NSDUH-2002-2017-RD16YR/crosstab/?control=STCTYCOD&row=CATAG2&column=AMIYR_U&filter=STCTYCOD%3D74%2C75%2C76%2C77&weight=DASWT_8&run_chisq=false&format=json
+<Response [500]>
+
+# this one fails because the column SMIYR_U doesn't exist for this year group
+index: 6, control: STCTYCOD, row: CATAG2, column: SMIYR_U,  filter: STCTYCOD%3D74%2C75%2C76%2C77, weight: DASWT_8
+https://rdas.samhsa.gov/api/surveys/NSDUH-2002-2017-RD16YR/crosstab/?control=STCTYCOD&row=CATAG2&column=SMIYR_U&filter=STCTYCOD%3D74%2C75%2C76%2C77&weight=DASWT_8&run_chisq=false&format=json
+<Response [500]>
+"""
 
 
 def load_state_and_county_data():
-    results = []
-    #get_all_nsduh_county(results) 
-    #get_all_nsduh_state(results)
-    get_nsduh_data(True, results)
-    get_nsduh_data(False, results)
+    results = set()
+    get_nsduh_data(True, results) # county
+    get_nsduh_data(False, results) # state
     write_json_to_csv_file(results)
-
 
 def getUrlData(index, base_url, control, row, column, filter, weight):
     local_url = f"{base_url}?control={control}&row={row}&column={column}&filter={filter}&weight={weight}&run_chisq=false&format=json"
@@ -160,39 +169,46 @@ def getUrlData(index, base_url, control, row, column, filter, weight):
 def get_nsduh_data(isCounty, results):
     print(f"in get_all_nsduh_data")
     try:
-        index = 0
-        base_url = state_base_url
-        ctl = state_control
-        fil = state_filter
-        wt = state_weight
+        # set default values as if we're getting state level data
         columns = state_columns
-        if isCounty:
-            base_url = county_base_url
-            ctl = county_control
-            fil = county_filter
-            wt = county_weight      
+        years = state_years
+        # if we're getting county date, reset default values appropriately
+        if isCounty:    
             columns = county_columns
-        for row in rows:
-            for column in columns:
-                index = index + 1
-                resp = getUrlData(index, base_url, ctl, row, column, fil, wt)
-                if resp.status_code == 200:
-                    parse_data(isCounty, json.loads(resp.text), results, True)
-                # if previous attempt didn't work and got a 400, it's likely throttled due to disclosure limitations,
-                # move control to row and drop the previous row which was a demographic thing
-                elif resp.status_code == 400 and resp.text == '{"errorCode":"DISCLOSURE_LIMITATION"}':
-                    print(f"resp.reason: {resp.reason}")
-                    print(f"resp.text: {resp.text}")
-                    resp = getUrlData(index, base_url, "", ctl, column, fil, wt)
+            years = county_years
+        # loop through all the different year ranges that we know of and get the data for them all
+        for year in years:
+            index = 0
+            url_term = year["url_term"]
+            base_url = f"https://rdas.samhsa.gov/api/surveys/{url_term}/crosstab/"
+            ctl = year["control"]
+            fil = year["filter"]
+            wt = year["weight"]
+            start_year = year["start_year"]
+            end_year = year["end_year"]
+            year_range = year["year_range"]
+            print(f"start_year: {start_year}, end_year: {end_year}, year_range: {year_range}")
+            for row in rows:
+                for column in columns:
+                    index = index + 1
+                    resp = getUrlData(index, base_url, ctl, row, column, fil, wt)
                     if resp.status_code == 200:
-                        parse_data(isCounty, json.loads(resp.text), results, False)
-                    elif resp.status_code == 400:
+                        parse_data(isCounty, json.loads(resp.text), results, True, start_year, end_year, year_range)
+                    # if previous attempt didn't work and got a 400, it's likely throttled due to disclosure limitations,
+                    # move control to row and drop the previous row which was a demographic thing
+                    elif resp.status_code == 400 and resp.text == '{"errorCode":"DISCLOSURE_LIMITATION"}':
                         print(f"resp.reason: {resp.reason}")
                         print(f"resp.text: {resp.text}")
+                        resp = getUrlData(index, base_url, "", ctl, column, fil, wt)
+                        if resp.status_code == 200:
+                            parse_data(isCounty, json.loads(resp.text), results, False, start_year, end_year, year_range)
+                        elif resp.status_code == 400:
+                            print(f"resp.reason: {resp.reason}")
+                            print(f"resp.text: {resp.text}")
     except Exception as err:
-        print(f"get_all_nsduh_state error: {err}")
+        print(f"get_nsduh_data error: {err}")
         
-    print(f"leaving get_all_nsduh_data")
+    print(f"leaving get_nsduh_data")
     #write_json_to_csv_file(results)
 
 def read_shape_file():
@@ -223,8 +239,8 @@ def read_shape_file():
             print(f"record: {rd['ST_NAME']},{rd['SR_NAME']},{rd['TXNPILA']},{rd['METAMYR']},{rd['PNRNMYR']},{rd['TXNOSPA']},{rd['TXNOSPI']},{rd['TXREC3']},{rd['UDPYILA']},{rd['UDPYILL']},{rd['UDPYPNR']}\n")
             #print(f"record: {rd['ST_NAME']}")
 
-# test method to show what we get back from the current url 
-def print_url_contents():
+# test method to show what we get back from a given url 
+def print_url_contents(url):
     print(f"print_url_contents: {url}")
     resp = req.get(url)
     print(f"{resp}\n\n") # this just prints "<Response [code num]>"
@@ -232,33 +248,30 @@ def print_url_contents():
         jsondata = json.loads(resp.text)
         print(jsondata)
 
-# test method which gets data from the current url and calls parse__data
-# make sure current url is the county one, not state one.
-def parse_current_url_data(isCounty, hasControl):
-    print(f"parse_current_url_data: {url}")
-    resp = req.get(url)
-    print(f"{resp}\n\n") # this just prints "<Response [code num]>"
-    if resp.status_code == 200:
-        jsondata = json.loads(resp.text)
-        results = []
-        parse_data(isCounty, jsondata, results, hasControl)
-        #write_json_to_csv_file(results)
-    else:
-        print(f"resp.reason: {resp.reason}")
-        print(f"resp.text: {resp.text}")
         
 def write_json_to_csv_file(results):
+    print("write_json_to_csv_file")
+    # used a set to store all the results in so it would weed out duplicates.  
+    # Sets can't store dicts, so made the dicts into tuples.  
+    # Now convert tuples back to dicts to write it out.  
+    # There's likely an easier way, but I wrote this as lists/dicts and discovered 
+    # I need to weed out the duplicates later, so this was the quickest way to fix that.
+    new_results = []
+    for r in results:
+        d = dict(r)
+        new_results.append(d)
+
     # write out to csv
-    keys = results[0].keys()
+    keys = new_results[0].keys()
     #print(f"keys: {keys}")
     data_file = open(csv_path, 'w', newline='')
     csv_writer = csv.DictWriter(data_file, fieldnames = keys) 
     csv_writer.writeheader() 
-    csv_writer.writerows(results) 
+    csv_writer.writerows(new_results) 
     data_file.close()
 
 
-def makeCellDict(county, row_type, col_type, row_value, col_value, count_unweighted, count_weighted):
+def makeCellDict(county, row_type, col_type, row_value, col_value, count_unweighted, count_weighted, start_year, end_year, year_range):
     #return f"'Hawaii', {county}, {row_type}, {col_type}, {row_value}, {col_value}, {count_unweighted}, {count_weighted}"
     return dict({
         "state": "Hawaii",
@@ -268,24 +281,25 @@ def makeCellDict(county, row_type, col_type, row_value, col_value, count_unweigh
         "row_value": row_value,
         "col_value": col_value,
         "count_unweighted": count_unweighted,
-        "count_weighted": count_weighted
+        "count_weighted": count_weighted,
+        "start_year": start_year,
+        "end_year": end_year,
+        "year_range": year_range,
     })
 
 # after moving control to row, dropping demographics, for throttled results, these still fail:
 # 3, 11, 19, 27
 # all of these use "ABODHER" as the column.  
-# TODO: I need to make sure results array removes duplicate results because 
-# we will get 4 duplicates for each time we drop the control
 #
 # Given jsondata, it loops through all the cells
 # - parses the data for each cell, pulling out what we want
 # - creates a json item for each set, formatting it so all entries will be consistent in spite of original json differences
-# this method is _messy_ aas it has to account for the json being formatted differently 
+# this method is _messy_ as it has to account for the json being formatted differently 
 # if the json was generated from a url with a control param or not.
 # results - storage for all the data I'm parsing out.  Gets modified directly, so there's no return value from this
 # hasControl - True if the jsondata was generated using a url that had a control parameter, 
 #              False if the control was moved to the row param
-def parse_data(isCounty, jsondata, results, hasControl):
+def parse_data(isCounty, jsondata, results, hasControl, start_year, end_year, year_range):
     print(f"in parse_data: hasControl: {hasControl}")
     try:
         jsondata = jsondata["results"]
@@ -303,9 +317,9 @@ def parse_data(isCounty, jsondata, results, hasControl):
                     county_value = ""
                     if isCounty:
                         county_value = counties[row_option]
-                    d = makeCellDict(county_value, "", col_dict["title"], "", col_dict[col_option], cell["count"]["unweighted"], cell["count"]["weighted"])
+                    d = makeCellDict(county_value, "", col_dict["title"], "", col_dict[col_option], cell["count"]["unweighted"], cell["count"]["weighted"], start_year, end_year, year_range)
                     #print(f"dict: {d}")
-                    results.append(d)
+                    results.add(tuple(d.items()))
                 else:
                     county_value = ""
                     control_option = cell["control_option"]
@@ -313,9 +327,9 @@ def parse_data(isCounty, jsondata, results, hasControl):
                         county_value = ""
                         if isCounty:
                             county_value = counties[control_option]
-                        d = makeCellDict(county_value, row_dict["title"], col_dict["title"], row_dict[row_option], col_dict[col_option], cell["count"]["unweighted"], cell["count"]["weighted"])
+                        d = makeCellDict(county_value, row_dict["title"], col_dict["title"], row_dict[row_option], col_dict[col_option], cell["count"]["unweighted"], cell["count"]["weighted"], start_year, end_year, year_range)
                         #print(f"dict: {d}")
-                        results.append(d) 
+                        results.add(tuple(d.items()))
         print("success")
     except Exception as err:
         print(f"parse_data error: error.text: error: {err} ")
@@ -337,7 +351,7 @@ def make_dict(jsondata, key):
     return d
 
 
-load_state_and_county_data()
+#load_state_and_county_data()
 #print_url_contents()
 #get_all_nsduh_state([])
 #get_all_nsduh_county([])
@@ -347,5 +361,26 @@ load_state_and_county_data()
 #parse_current_url_county_data(False)
 #parse_current_url_state_data(False)
 
-#read_shape_file()
+read_shape_file()
 
+
+""" 
+# test method which gets data from the current url and calls parse__data
+# make sure current url is the county one, not state one.
+
+# currently does not work since I change the results object to a set and 
+# added in the start_year, end_year, and year_range
+
+def parse_current_url_data(isCounty, hasControl):
+    print(f"parse_current_url_data: {url}")
+    resp = req.get(url)
+    print(f"{resp}\n\n") # this just prints "<Response [code num]>"
+    if resp.status_code == 200:
+        jsondata = json.loads(resp.text)
+        results = []
+        parse_data(isCounty, jsondata, results, hasControl)
+        #write_json_to_csv_file(results)
+    else:
+        print(f"resp.reason: {resp.reason}")
+        print(f"resp.text: {resp.text}") 
+"""
